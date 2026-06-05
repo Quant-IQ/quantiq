13302d48ee2b136b3e46aa36fb55c004c8cf7dd5
sam18-VK <samarth18.srivastava@gmail.com>
2026-06-05 23:55:12 +0530

    Add dynamic watchlist scaffold

--- src/watchlist/dynamic.py
+++ src/watchlist/dynamic.py
@@ -1,2 +1,20 @@
 class DynamicWatchlist:
     pass
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class DynamicWatchlist:
+    def __init__(self, config_name: str):
+        self.config_name = config_name
+        self.tickers = []
+
+        logger.info(
+            f"Created dynamic watchlist for config: {config_name}"
+        )
+
+    def refresh(self):
+        raise NotImplementedError(
+            "Waiting for screener interface"
+        )
